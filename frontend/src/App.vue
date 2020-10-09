712cdcb9227d8b115e1dc6e058ddbf0bcac4138d
prafesar <prafesar@tut.by>
2020-10-09 12:29:05 +0300

    remove comments from App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,18 +2,15 @@
   <div>
     <page-header />
     <router-view />
-    <!-- <page-footer /> -->
   </div>
 </template>
 
 <script>
 import PageHeader from '@/components/PageHeader';
-// import PageFooter from '@/components/PageFooter';
 
 export default {
   components: {
-    PageHeader,
-    // PageFooter
+    PageHeader
   }
 }
 </script>
